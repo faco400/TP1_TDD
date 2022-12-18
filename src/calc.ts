40b103a4e330baefbe46294fb65a2fe8635ecaed
Wagner M Cunha <wagnermc506@gmail.com>
2022-12-17 23:09:55 -0300

    duplicacao: ambos testes do calculo da base de calculo passando

--- src/calc.ts
+++ src/calc.ts
@@ -1,5 +1,9 @@
+function sum(list: number[]): number {
+    return list.reduce((acc, value) => acc + value, 0)
+}
+
 function calculateBaseValue(incomeValues: number[], deductions: number[]): number {
-    return 1000
+    return sum(incomeValues) - sum(deductions)
 }
 
 export {calculateBaseValue}
