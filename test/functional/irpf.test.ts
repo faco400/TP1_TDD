fb42958d56d605e3307194a8ee8eb858357d2755
Wagner M Cunha <wagnermc506@gmail.com>
2022-12-17 23:21:39 -0300

    testes refatorados para um único teste parametrizado

--- test/functional/irpf.test.ts
+++ test/functional/irpf.test.ts
@@ -10,20 +10,10 @@ describe('Beach IRPF functional tests', () => {
   });
 });
 
-test('Calculate base value of tax with one income and without deductions', () => {
-  const income: number[] = [1000.0]
-  const deduction: number[] = []
-  expect(calculateBaseValue(income, deduction)).toBe(1000.0)
-})
-
-test('Calculate base value of tax with more than one income and one deduction', () => {
-  const income: number[] = [2000.0, 3000.0]
-  const deduction: number[] = [1000.0]
-  expect(calculateBaseValue(income, deduction)).toBe(4000.0)
-})
-
-test('Calculate base value of tax with more than one income and more than one deduction', () => {
-  const income: number[] = [1000.0, 2000.00]
-  const deduction: number[] = [500.0, 200.0]
-  expect(calculateBaseValue(income, deduction)).toBe(2300.0)
+test.each([
+  [[1000.0], [], 1000.0],
+  [[2000.0, 3000.0], [1000.0], 4000.0],
+  [[1000.0, 2000.0], [500.0, 200.0], 2300.0]
+])("Calculate base value of tax", (incomes, deductions, expected) => {
+  expect(calculateBaseValue(incomes, deductions)).toBe(expected)
 })
