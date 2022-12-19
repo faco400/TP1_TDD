d7220260eac359ed419451860adae653dde1d131
Wagner M Cunha <wagnermc506@gmail.com>
2022-12-19 10:44:46 -0300

    triangulacao: teste para o calculo do valor do imposto por faixa

--- test/functional/irpf.test.ts
+++ test/functional/irpf.test.ts
@@ -37,3 +37,9 @@ test('Calculate IPRF per range with value on the second range', () => {
   const expected = [0, 29.03, 0, 0, 0]
   expect(calculateIRPFTaxPerRange(rangeValues, table_2022)).toEqual(expected.map((value)=> expect.closeTo(value, 2)))
 })
+
+test('Calculate IPRF per range with value on the last range', () => {
+  const rangeValues = [1903.98, 922.67, 924.40, 913.63, 4506.93]
+  const expected = [0, 69.20, 138.66, 205.57, 1239.41]
+  expect(calculateIRPFTaxPerRange(rangeValues, table_2022)).toEqual(expected.map((value)=> expect.closeTo(value, 2)))
+})
