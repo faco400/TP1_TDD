7f1f4dee18605ec1144d10d4545fbebb9c996d9e
faco400 <vd.souza1999@gmail.com>
2022-12-21 09:39:55 -0300

    Adicionando test pro duplicacao registerdeduction

--- src/deductionsRegister.ts
+++ src/deductionsRegister.ts
@@ -4,22 +4,42 @@ export interface IDeduction {
 }
 
 const deductionlist: IDeduction[] = []
+export interface DeductionResponse {
+  statusCode: number,
+  response: IDeduction[],
+  totalDeduction: number
+}
+
+export function calculateDeductionTotals (deductionlist: IDeduction[]){
+  try{
+    const totalDeduction = deductionlist.reduce(function (accumulator, deduction) {
+      return accumulator + deduction.value;
+    }, 0)
+    return totalDeduction
+  
+  }catch (error){
+    return error
+  }
+}
 
 export function createDeduction(deduction: IDeduction) {
   try{
-    const deduction = {
-      value: 5000,
-      description: 'Previdencia Privada',
-    }
     if (deduction.value < 0)
       throw new Error('ValorRendimentoInvalidoException')
     if (deduction.description === "")
       throw new Error('DescricaoEmBrancoException')
-    return deduction;
+
+    deductionlist.push(deduction)
+    const total = calculateDeductionTotals(deductionlist)
+
+    const res_data: DeductionResponse = {
+      statusCode: 200,
+      response: deductionlist,
+      totalDeduction: Number(total)
+    }
+    return res_data;
+  
   }catch(error){
     return error;
   }
-  return{
-    
-  } 
 }
--- test/functional/irpf.test.ts
+++ test/functional/irpf.test.ts
@@ -1,6 +1,6 @@
 import app from '@src/index';
 import request from 'supertest';
-import {createDeduction, IDeduction} from '@src/deductionsRegister'
+import {createDeduction, IDeduction, calculateDeductionTotals} from '@src/deductionsRegister'
 
 describe('IRPF functional tests', () => {
   test('Should register a deduction', () => {
@@ -8,9 +8,36 @@ describe('IRPF functional tests', () => {
       value: 5000,
       description: 'Previdencia Privada',
     }
-    expect(createDeduction(deduction)).toEqual({
+
+    const res: any = createDeduction(deduction)
+    expect(res.statusCode).toEqual(200)
+    expect(res.response.pop()).toEqual({
       value: 5000,
       description: 'Previdencia Privada',
     })
+
+    expect(res.totalDeduction).toEqual(5000)
+  })
+
+  test('Should register more than onde deduction', () => {
+    const deductionlist: IDeduction[] = [
+      {
+        value: 500,
+        description: 'Pensao alimenticia',
+      },
+      {
+        value: 2000,
+        description: 'Despesas com educacao',
+      }
+    ]
+    let totalDeduction = 0;
+    deductionlist.map((deduction) => {
+      const res: any = createDeduction(deduction);
+      expect(res.statusCode).toEqual(200);
+      const lastdeduction = res.response.pop()
+      expect(lastdeduction).toEqual(deduction)
+      totalDeduction += res.totalDeduction
+    })
+    expect(totalDeduction).toEqual(calculateDeductionTotals(deductionlist))
   })
 });
