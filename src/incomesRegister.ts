b71ba93eee1823e4b9cde746d9073078a53671df
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-12-20 23:59:12 -0300

    feat: adicionando teste de duplicação
    Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>

--- src/incomesRegister.ts
+++ src/incomesRegister.ts
@@ -14,6 +14,7 @@ export function calculateIncomeTotals(data: IIncome[]) {
         const totalIncome = data.reduce(function (accumulator, income) {
             return accumulator + income.value
         }, 0);
+        console.log("total income", totalIncome);
         return totalIncome;
     } catch (error) {
         return error
@@ -22,23 +23,13 @@ export function calculateIncomeTotals(data: IIncome[]) {
 
 export function registerIncome(income: IIncome) {
     try {
-        const income: IIncome = {
-            value: 3000,
-            description: "Dividendos recebidos por meio de fundos imobiliários"
-        }
         if (income.value <= 0) throw new Error('ValorRendimentoInvalidoException');
         if (income.description === "") throw new Error('DescricaoEmBrancoException');
         data.push(income);
-        const total = calculateIncomeTotals([{
-            value: 3000,
-            description: "Dividendos recebidos por meio de fundos imobiliários"
-        }]);
+        const total = calculateIncomeTotals(data);
         const res_data: IResponse = {
             statusCode: 200,
-            response: [{
-                value: 3000,
-                description: "Dividendos recebidos por meio de fundos imobiliários"
-            }],
+            response: data,
             totalIncomes: Number(total)
         }
         return res_data;
--- test/functional/irpf.test.ts
+++ test/functional/irpf.test.ts
@@ -1,4 +1,4 @@
-import { IIncome, registerIncome } from '@src/incomesRegister';
+import { IIncome, registerIncome, calculateIncomeTotals } from '@src/incomesRegister';
 
 describe('Income tests', () => {
   test('Should create income by falsificate data', () => {
@@ -13,5 +13,31 @@ describe('Income tests', () => {
       description: "Dividendos recebidos por meio de fundos imobiliários"
     });
     expect(res.totalIncomes).toEqual(3000);
+  });
+
+  test('Should create a income by duplicate data', () => {
+    const data: IIncome[] = [
+      {
+        value: 4000,
+        description: "Rendimento referente ao salário.",
+      },
+      {
+        value: 1000,
+        description: "Rendimento dividendos.",
+      },
+      {
+        value: 1500,
+        description: "Renda Extra",
+      }
+    ];
+    let totalIncome = 0;
+    data.map((income) => {
+      const res: any = registerIncome(income);
+      expect(res.statusCode).toEqual(200);
+      const lastIncome = res.response.pop()
+      expect(lastIncome).toEqual(income)
+      totalIncome += res.totalIncomes;
+    })
+    expect(totalIncome).toEqual(calculateIncomeTotals(data))
   });
 });
